c02ce037868815dc2dad3ef0a9ca355b583d8261
Tom <thomas.zumsteg@gmail.com>
2014-04-27 10:29:45 -0700

    Problem123

---
+++ Problem123.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""http://projecteuler.net/problem=123"""
+
+from time import time
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    start = time()
+    main()
+    print "That took %f seconds" %(time() - start)
--- problem122.py
+++ problem122.py
@@ -2,35 +2,82 @@
 """http://projecteuler.net/problem=122"""
 
 from time import time
-from sys import stdout
 from itertools import combinations_with_replacement as cwr
+from sys import stdout
 
 def main():
-	m_sum = 0
-	for k in range(1,201):
-		m = m_func(k)
-		print "%d: %d" %(k,m)
-		m_sum += m
-	print "The sum is %d" %(m_sum)
+    limit = 200
+    seen = [0,0] + [None] * (limit - 1)
+    queue = [[1]]
+    while None in seen:
+        node = queue.pop(0)
+        stdout.write("%d:%d:%d\r" %(seen.count(None),len(node),len(queue)))
+        stdout.flush()
+        for new_node in node_gen(node):
+            if new_node[-1] <= limit:
+                if seen[new_node[-1]] == None:
+                    seen[new_node[-1]] = len(new_node) - 1
+                queue.append(new_node)
+    print "\nsum is %d" %(sum(seen))
 
-def m_func(num):
-	if num == 1: return 0
-	qout = set([frozenset([1])])
-	step = 1
-	while True:
-		qin  = qout.copy()
-		qout = set()
-		for s in qin:
-			for (i,j) in cwr(s,2):
-#				print "%d: %d" %(i,j)
-				ij = i + j
-				if ij < num:
-					qout.add(s | set([ij]))
-				elif ij == num:
-					return step
-		step += 1
+def node_gen(node):
+    lim = node[-1]
+    done = set()
+    for i,n in reversed(zip(range(1,len(node)+1),node)):
+        for m in reversed(node[:i]):
+            #print "%d, %d" %(n,m)
+            mn = m + n
+            if mn <= lim: break
+            elif mn not in done:
+                yield node + [mn]        
+                done.add(mn)
 
-if __name__ == "__main__":
-	start = time()
-	main()
-	print "That took %f seconds" %(time() - start)
+def j_gen(i):
+    for j in range(i+1):
+        yield 2**j
+
+def slow_main(k):
+    nums_seen = [0,0] + [None] * (300-1)
+    new = set([frozenset([1])])
+    step = 1
+    for step in range(1,k+1):
+        #stdout.write("%d: %d\r" %(step, nums_seen.count(None)))
+        #stdout.flush()
+        old = prune(new, step)
+        new = set()
+        for node in old:
+            for (n,new_node) in node_gen(node):
+                new.add(new_node)
+                try:
+                    if nums_seen[n] == None:
+                        nums_seen[n] = step
+                        #print "%d: %d" %(n,step)
+                except IndexError: pass
+    see_nums(nums_seen)
+
+def see_nums(nums):
+    for s in range(max(nums)):
+        string = "%d:" %s
+        for i,n in enumerate(nums):
+            if n == s:
+                string += " %d" %i
+        print string
+
+def prune(s,n):
+    t = set()
+    for i in s:
+        if len(i) == n:
+            t.add(i)
+    return t
+
+#def node_gen(node):
+#    for a,b in cwr(node,2):
+#        s = frozenset(set([a+b]) | node)
+#        yield (a+b, s)
+
+if __name__=="__main__":
+    start = time()
+    main()
+    #for a,s in node_gen(frozenset([1,2])):
+    #    print "%d: %s" %(a,str(s))
+    print "That took %f seconds" %(time() - start)
