6d272eefe451ae791ee848b127a1575e4d212a1a
Tom <thomas.zumsteg@gmail.com>
2014-05-17 21:33:46 -0700

    Finished 130

--- common.py
+++ common.py
@@ -2,6 +2,21 @@
 from math import sqrt
 from re import sub
 
+def is_prime(n):
+	if n<2:
+		return False
+	if n in (2,3):
+		return True
+	if n % 2 == 0 or n % 3 == 0:
+		return False
+	max_divisor = int(n**0.5)+1
+	divisor = 5
+	while divisor <= max_divisor:
+		if n % divisor == 0 or n % (divisor + 2) == 0:
+			return False
+		divisor += 6
+	return True
+
 def prime_factors(num):
 	factors = []
 	for prime in prime_generator():
