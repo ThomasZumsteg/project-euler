54a0895cecfd296c7457be61d38793f548cdb398
Tom <thomas.zumsteg@gmail.com>
2014-05-20 15:11:45 -0700

    Start of 134

--- problem134.py
+++ problem134.py
@@ -3,30 +3,36 @@
 
 from time import time
 from sys import stdout
-from common import prime_generator
+from common import prime_generator, is_prime
 from itertools import count
 
 def main():
 	big_num = 1000000
 	p_big = 1
 	s_sum = 0
-	for p in prime_generator(block = big_num + 100):
-		(p_big, p_small) = (p, p_big)
-		if p_big - p_small != 2: continue
-		if p_big == 5: continue
-		if p_small > big_num: break
-		stdout.write("%d: %d\r" %(p_big, p_small))
+	for (large, small) in prime_pairs(big_num):
+		if large == 5: continue
+		stdout.write("%d: %d\r" %(large, small))
 		stdout.flush()
-		s_sum += find_s(p_small, p_big)
-	print "The s sum is %d" %(s_sum)
+		s_sum += get_s(small, large)
 
-def find_s(p1, p2):
-	rem = p1
+def prime_pairs(lim, block = 1000100):
+	pg = prime_generator(block)
+	large = pg.next()
+	for p in pg:
+		(large, small) = (p, large)
+		if small > lim: return
+		if large - small == 2:
+			yield (large, small)
+			
+def get_s(p1, p2):
 	ord = get_ord(p1)
-	for n in count(1):
-		rem = (ord + rem) % p2
-		#print rem
-		if rem == 0: return n
+	(m, n) = diophantine(p2, -ord, p1)
+	return n
+
+def diophantine(a, b, c):
+
+	
 
 def get_ord(n):
 	for i in count(1):
