5252ab32394810ba5bb6658dc494f35e3e7fc63a
Tom <thomas.zumsteg@gmail.com>
2014-05-21 17:28:13 -0700

    Solved 134

--- problem134.py
+++ problem134.py
@@ -7,35 +7,67 @@ from common import prime_generator, is_prime
 from itertools import count
 
 def main():
+	#big_num = 10000
 	big_num = 1000000
-	p_big = 1
 	s_sum = 0
-	for (large, small) in prime_pairs(big_num):
-		if large == 5: continue
+	pg = prime_generator(big_num + 1000)
+	large = pg.next()
+	large = pg.next()
+	large = pg.next()
+	#print large
+	for p in pg:
+		(large, small) = (p, large)
+		if small > big_num: break
 		stdout.write("%d: %d\r" %(large, small))
 		stdout.flush()
-		s_sum += get_s(small, large)
+		s_sum += get_s(large, small) 
+		#if get_s(large,small) != slow_get_s(large, small):
+		#	print "Problem: (%d, %d)" %(large,small)
+	print "Sum of all s is %d" %(s_sum)
 
 def prime_pairs(lim, block = 1000100):
 	pg = prime_generator(block)
 	large = pg.next()
 	for p in pg:
 		(large, small) = (p, large)
-		if small > lim: return
+		if small >= lim: return
 		if large - small == 2:
 			yield (large, small)
 			
-def get_s(p1, p2):
+def get_s(p2, p1):
 	ord = get_ord(p1)
-	(m, n) = diophantine(p2, -ord, p1)
-	return n
+	(_, (b, a)) = diophantine(p2, -ord, p1)
+	#print "(%d, %d)" %( b, a)
+	return (b * -int(a//b) + a) * ord + p1
 
-def diophantine(a, b, c):
-	rem = [a, b]
+def slow_get_s(p2, p1):
+	ord = get_ord(p1)
+	rem = p1
+	for n in count(0):
+		num = ord * n + p1
+		if num % p2 == 0:
+			return num
+
+def diophantine(x,y,z):
+	# solve alfa * x + beta * y = z for integers alfa and beta
+	# beta = f(a_m, a_b) = b_m * i + b_b
+	# alfa = f(a_m, b_b) = a_m * i + a_b
+	q = []
+	(d, r) = (x, y)
 	for i in count(0):
-		rem.append(rem[i] % rem[i+1])
-		if rem[-1] == 0: break
-	print rem
+		q.append(d//r)
+		#print "%d = %d * %d + %d" %(d, q[-1], r, d % r)
+		(r, d) = (d % r, r)
+		if r == 0: break
+	# d is now gcf(x,y)
+	(big, small) = (0,1)
+	for n in reversed(q[:-1]):
+		(big, small) = (small, big - small * n)
+		#print "(%d): %d: %d" %(n, big, small)
+	#print "big: %d. small: %d" %(z // d * big, z // d * small)
+	#print "%d = (%d + %d * n) * %d + (%d + %d * n) * %d" %(z, z // d * big, y//d, x, z // d * small, -x // d, y)
+	#        (   a_m,          a_b), (   b_m,            b_b)
+	return ((y // d, z // d * big), (-x // d, z // d * small))
 
 def get_ord(n):
 	for i in count(1):
@@ -44,6 +76,6 @@ def get_ord(n):
 
 if __name__ == "__main__":
 	start = time()
-#	main()
-	diophantine(-100,23,19)
+	main()
+	#print get_s(23,19)
 	print "That took %f seconds" %(time() - start)
