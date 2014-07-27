74f598120b26c29d250ae6ecb50cef3b4407f091
Tom <thomas.zumsteg@gmail.com>
2014-07-26 21:44:33 -0700

    Start 139 (scaffolding only)

---
+++ problem139.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""Finds the number of integer right trangles with perimeter less then one-hundered million that can be tiled to create an square integer area
+Further explination: http://projecteuler.net/problem=139"""
+
+from time import time
+
+def main():
+	return 0
+
+if __name__ == "__main__":
+	start = time()
+	print "The answer is %d" % main()
+	print "That took %f second" %(time() - start)
