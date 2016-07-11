30e9e49105ff3b123ca2cc4b14e4426157e3cab4
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-07-10 21:30:44 -0700

    Solves problem 0001 in haskell

--- problem_0001.hs
+++ problem_0001.hs
@@ -3,7 +3,10 @@ module Main where
 import Text.Printf
 
 problem0001 :: Int -> [Int] -> Int
-problem0001 limit factors = sum (filter (any ) [1..limit])
+problem0001 limit factors = sum ( filter ( divisibleByAny factors ) [1..( limit - 1)])
+
+divisibleByAny :: [Int] -> Int -> Bool
+divisibleByAny factors num = any (\f -> mod num f == 0) factors
 
 main = do
-    print (problem0001 100 [3, 5])
+    print (problem0001 1000 [3, 5])
