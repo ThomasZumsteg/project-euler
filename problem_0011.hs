5687265036d595492ae867b058ff3685ce5b7ad5
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-07-26 17:36:16 -0700

    more work on problem 11 in haskell

--- problem_0011.hs
+++ problem_0011.hs
@@ -5,32 +5,32 @@ import System.Console.CmdArgs
 import Data.Time
 import System.IO
 import Data.Char
+import Test.HUnit
 
 -- In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
 -- The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
 -- What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
 
-data Direction = All | DiagonalOnly
+data Direction = Horizontal | Vertical | DiagonalUp | DiagonalDown
 
 data EulerArgs = 
     Diagonal { path :: String }
+    | UnitTests 
     | Euler 
         deriving (Show, Data, Typeable)
     
 diag = Diagonal { path = "problem_0011.txt" }
+unittest = UnitTests{}
 euler = Euler{}
 
-problem0011 :: Direction -> String -> Integer
-problem0011 All path = maximum $ (diagonal ++ horizontal ++ vertical)
-    where
-        grid = do parseFile path
-        diagonal = [1..10]
-        horizontal = [1..10]
-        vertical= [1..10]
-problem0011 DiagonalOnly path = maximum $ diagonal
-    where
-        grid = do parseFile path
-        diagonal = [1..10]
+problem0011 :: String -> [Direction] -> Integer
+problem0011 path = map (getSlices grid) 
+
+getSlices :: (Integral a) => [[a]] -> Direction -> [[a]]
+getSlices grid Horizontal = grid
+getSlices grid Vertical = [[]]
+getSlices grid DiagonalDown = [[]]
+getSlices grid DiagonalUp = [[]]
 
 parseFile :: String -> IO [[Integer]]
 parseFile name = do
@@ -38,14 +38,20 @@ parseFile name = do
     contents <- hGetContents file
     return $ map (map read . words) $ lines contents
 
-exec :: EulerArgs -> Integer 
-exec Diagonal{..} = problem0011 DiagonalOnly path
-exec Euler = problem0011 All "problem_0011.txt"
+exec :: EulerArgs -> IO ()
+exec Diagonal{..} = print $ problem0011 DiagonalOnly path
+exec Euler = print $ problem0011 All "problem_0011.txt"
+exec UnitTests = do
+    runTestTT $ TestList tests
+    return ()
+
+tests :: [Test]
+tests = [ TestCase $ ]
 
 main :: IO ()
 main = do
-    args <- cmdArgs $ modes [diag, euler]
+    args <- cmdArgs $ modes [diag, unittest, euler]
     start <- getCurrentTime
-    print $ exec args
+    exec args
     stop <- getCurrentTime
     print $ diffUTCTime stop start
