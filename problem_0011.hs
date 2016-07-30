36cde2e84abd755709a931a7c49893ca9a912f8f
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-07-29 21:41:46 -0700

    More work on problem 11

--- problem_0011.hs
+++ problem_0011.hs
@@ -24,39 +24,27 @@ diag = Diagonal { path = "problem_0011.txt" }
 unittest = UnitTests{}
 euler = Euler{}
 
-problem0011 :: String -> [Direction] -> Integer
-problem0011 path ds = 0
+problem0011 :: (Integral a) => [[a]] -> a -> [Direction] -> Maybe a
+problem0011 [[]] _ _ = error "Empty matrix"
+problem0011 _ [] _ = Nothing
+problem0011 grid l dirs = maximum $ map (maximum . getSlices grid l) dirs
 
-data Coord = Coord { row :: Int, col :: Int }
+getSlices :: (Integral a) => [[a]] -> a -> Direction -> a
+getSlices _ _ _ = 0
 
-getSlices :: (Integral a) => Direction -> [[a]] -> [[a]]
-getSlices Horizontal = id
-getSlices Vertical = transpose
-getSlices DiagonalDown = map (getCoords diagonalDown) 
-    where
-        getCoords Coord{row=r, col=c} matrix = matrix !! r !! c
-getSlices DiagonalUp = map (getCoords diagonalUp)
-    where
-        getCoords Coord{row=r, col=c} matrix = matrix !! r !! c
+testGridSmall :: [[Integer]]
+testGridSmall = [[1,2],[3,4]]
 
-diagonalDown :: [[Coord]]
-diagonalDown = map ( map (\(r, c) -> Coord{row=r,col=c}) ) [ [(0,1)],
-    [(0,0), (1,1)],
-    [(1,0)] ]
+testGridLarge :: [[Integer]]
+testGridLarge = [[n..(n+9)] | n <- [0,10..90]]
 
-diagonalUp :: [[Coord]]
-diagonalUp = map (map (\(r, c) -> Coord{row=r,col=c})) [ [(0,0)],
-    [(1,0), (0,1)],
-    [(1,1)] ]
-
-getSlicesTests :: [Test]
-getSlicesTests = map TestCase 
-    [ [[]] @=? getSlices Horizontal [[]],
-    [[1,2], [3,4]] @=? getSlices Horizontal [[1,2],[3,4]],
-    [[1,3], [2,4]] @=? getSlices Vertical [[1,2],[3,4]],
-    [[1], [3,2], [4]] @=? getSlices DiagonalUp [[1,2],[3,4]],
-    [[3], [1,4], [2]] @=? getSlices DiagonalDown [[1,2],[3,4]]
-    ]
+problem0011Test :: [Test]
+problem0011Test = map TestCase 
+    [ Just 1 @=? problem0011 [[1]] 1 [Vertical] ,
+    Just 1 @=? problem0011 [[1]] 1 [Horizontal] ,
+    Just 1 @=? problem0011 [[1]] 1 [DiagonalUp] ,
+    Just 1 @=? problem0011 [[1]] 1 [DiagonalDown]
+    ] 
 
 parseFile :: String -> IO [[Integer]]
 parseFile name = do
@@ -65,10 +53,18 @@ parseFile name = do
     return $ map (map read . words) $ lines contents
 
 exec :: EulerArgs -> IO ()
-exec Diagonal{..} = print $ problem0011 path [DiagonalUp, DiagonalDown]
-exec Euler = print $ problem0011 "problem_0011.txt" [DiagonalUp, DiagonalDown, Horizontal, Vertical]
-exec UnitTests = do
-    runTestTT $ TestList getSlicesTests
+exec Diagonal{..} = do
+    grid <- parseFile path
+    let directions = [DiagonalUp, DiagonalDown]
+    result <- problem0011 grid 4 directions 
+    print result
+exec Euler = do
+    grid <- parseFile "problem_0011.txt"
+    let directions = [DiagonalUp, DiagonalDown, Horizontal, Vertical]
+    result <- problem0011 grid 4 directions
+    print result
+exec UnitTests = do 
+    runTestTT $ TestList problem0011Test
     return ()
 
 main :: IO ()
