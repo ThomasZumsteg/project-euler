49e24c7cc0d5d08c033fd20a89cc6dea641fd3ca
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-08-04 21:09:32 -0700

    Update problem 11 to python 3

--- problem_0011.py
+++ problem_0011.py
@@ -1,66 +1,68 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 """What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20x20 grid? (see problem 11.txt)"""
 
 from re import sub
+from operator import mul
+from time import time
 
 def main():
-	data_set = read_file("problem11.txt")
-	print find_largest_multiple_of_n(data_set,4)
-	
+    data_set = read_file("problem_0011.txt")
+    print(find_largest_multiple_of_n(data_set,4))
+
 def read_file(file_name):
-	file_handle = open(file_name)
-	data = []
-	for line in file_handle:
-		line = line.split(' ')
-		line = [int(x) for x in line]
-		data.append(line)
-	return data
+    data = []
+    with open(file_name, 'r') as f:
+        for line in f.readlines():
+            data.append([int(x) for x in line.split(' ')])
+    return data
 
 def find_largest_multiple_of_n(data,n):
-	max_multiple = 0
-	for i in range(len(data)):
-		for j in range(len(data[i])):
-			this_multiple = evaluate(data,j,i,n)
-			if this_multiple > max_multiple:
-				max_multiple = this_multiple
-	return max_multiple
+    max_multiple = 0
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            this_multiple = evaluate(data,j,i,n)
+            if this_multiple > max_multiple:
+                max_multiple = this_multiple
+    return max_multiple
 
 def evaluate(data,horz,vert,length):
-	multiply = lambda x,y: x*y
-	[horz_mult, diag_mult, vert_mult, udag_mult] = [None]*4
+    horz_mult, diag_mult, vert_mult, udag_mult = 0, 0, 0, 0
 
-#	for line in data[vert:]:
-#		print line[horz:]
-#	print ""
+    if len(data[vert])-horz >= length:
+        horz_mult = reduce(mul, data[vert][horz:horz+length])
+    if len(data[vert])-horz >= length and len(data)-vert >= length:
+        diag_mult = reduce(mul, get_diag(data, horz, vert, length))
+        udag_mult = reduce(mul, get_udag(data, horz, vert, length))
+    if len(data)-vert >= length:
+        vert_mult = reduce(mul, get_vert(data, horz, vert, length))
+    return max(diag_mult, vert_mult, horz_mult, udag_mult)
 
-	if len(data[vert])-horz >= length:
-		horz_mult = reduce(multiply, data[vert][horz:horz+length])
-	if len(data[vert])-horz >= length and len(data)-vert >= length:
-		diag_mult = reduce(multiply, get_diag(data, horz, vert, length))
-		udag_mult = reduce(multiply, get_udag(data, horz, vert, length))
-	if len(data)-vert >= length:
-		vert_mult = reduce(multiply, get_vert(data, horz, vert, length))
-	
-	return max(diag_mult, vert_mult, horz_mult, udag_mult)
+def reduce(operator, data):
+    result = data[0]
+    for d in data[1:]:
+        result = operator(result, d)
+    return result
 
 def get_vert(data, horz, vert, length):
-	vert_list = []
-	for i in range(length):
-		vert_list.append(data[vert+i][horz])
-	return vert_list
+    vert_list = []
+    for i in range(length):
+        vert_list.append(data[vert+i][horz])
+    return vert_list
 
 def get_diag(data, horz, vert, length):
-	diag_list = []
-	for i in range(length):
-		diag_list.append(data[vert + i][horz + i])
-	return diag_list
+    diag_list = []
+    for i in range(length):
+        diag_list.append(data[vert + i][horz + i])
+    return diag_list
 
 def get_udag(data, horz, vert, length):
-	udag_list = []
-	for i in range(length):
-		udag_list.append(data[vert + length - 1 - i][horz + i ])
-	return udag_list
+    udag_list = []
+    for i in range(length):
+        udag_list.append(data[vert + length - 1 - i][horz + i ])
+    return udag_list
 
 if __name__ == "__main__":
-	main()
+    start = time()
+    main()
+    print('That took {:0.5f} seconds'.format(time() - start))
