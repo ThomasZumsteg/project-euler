059a5067790175beab289ef4457c35237ac0ab0b
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-08-14 15:53:20 -0700

    Updated with timings and better python

--- problem_0013.py
+++ problem_0013.py
@@ -2,13 +2,14 @@
 
 """Work out the first ten digits of the sum of the following one-hundred 50-digit numbers."""
 
-def main():
-	file_handle = open("problem_0013.txt")
-	nums = []
-	for line in file_handle:
-		nums.append(int(line))
-	nums_sum = str(sum(nums))
-	print(nums_sum[:10])
+from time import time
 
-if __name__ == "__main__": 
-	main()
+def main():
+    with open("problem_0013.txt") as f:
+        total = sum(int(l) for l in f.readlines())
+    print('Answer: {}'.format(str(total)[:10]))
+
+if __name__ == "__main__":
+    start = time()
+    main()
+    print('That took {:1.5f} seconds'.format(time() - start))
