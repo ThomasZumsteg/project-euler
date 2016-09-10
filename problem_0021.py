14bae3ff0d3d707713bbb772b50fad60d3df2920
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-09-09 22:17:18 -0700

    updated problem 21 to python3

--- problem_0021.py
+++ problem_0021.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 from common import factors_of_num
+from time import time
 
 def main():
-	amicable_sum = 0
-	for num in range(10000):
-		if is_amicable(num):
-			amicable_sum += num
-	print amicable_sum 
+    amicable_sum = 0
+    for num in range(10000):
+        if is_amicable(num):
+            amicable_sum += num
+    print('Answer: {}'.format(amicable_sum))
 
 def is_amicable(num):
-	a_pair = sum(factors_of_num(num))
-	if a_pair == num: return False
-	b_pair = sum(factors_of_num(a_pair))
-	if b_pair == num: 
-		print "%d :%d" %(a_pair, b_pair)
-		return True
-	return False
+    a_pair = sum(int(n) for n in factors_of_num(num))
+    if a_pair == num: return False
+    b_pair = sum(int(n) for n in factors_of_num(a_pair))
+    return b_pair == a_pair
 
 if __name__ == "__main__":
-	main()
+    start = time()
+    main()
+    print('That took {:0.04f} seconds'.format(time() - start))
