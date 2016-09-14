6479a81af4893e5c0287c71b5b7b7fc8fb4fa886
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-09-13 22:32:32 -0700

    Work on problem 21 in haskell

--- problem_0021.hs
+++ problem_0021.hs
@@ -8,7 +8,50 @@
 -- Evaluate the sum of all the amicable numbers under 10000.
 
 import Common
+import qualified Data.Map.Lazy as M
 
-main = euler_main [AdHoc 100 "Hello World", Euler 101, []]
+problem0021 :: Integer -> Integer
+problem0021 limit = foldl addPairs $ takeWhile belowLimit amicablePairs
+    where
+        addPairs acc (a, b) = acc + a + b 
+        belowLimit (a, b) = a < limit && b < limit
 
+problem0021Test = map TestCase [ ] 
+
+amicablePairs :: (Integral a) => [(a, a)]
+amicablePairs = amicablePairsWorker M.null 1
+
+amicablePairsTest = map TestCase [ ]
+
+amicablePairsWorker :: (Integral a) => M.Map a [a] -> a -> [(a, a)]
+amicablePairsWorker cache n = pairs : more
+    where
+        factorSum = sum $ factors n
+        pairs = map (\n' -> (n', n)) (M.findWithDefault [] n cache)
+        cache' = error "Not Implemented"
+        more = amicablePairsWorker 
+
+amicablePairsWorkerTest = map TestCase [ ]
+
+factors :: (Integral a) => a -> [a]
+factors n = error "Not implemented"
+
+factorsTest :: [Test]
+factorsTest = map TestCase [
+    [1] @=? factors 1,
+    [1,2] @=? factors 2,
+    [1,2] @=? factors 4,
+    [1,2,4,5,10,11,20,22,44,55,110] @=? factors 220
+    ]
+
+unitTests = problem0021Test ++ 
+    amicablePairsWorkerTest ++ 
+    amicablePairsTest ++ 
+    factorsTest
+
+main = euler_main [
+    AdHoc 100 "Hello World %d\n",
+    Euler 101,
+    UnitTest unitTests
+    ]
 
