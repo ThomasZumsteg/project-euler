007f10e31960a304ca224958e8e8a053b07d554b
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-09-21 21:29:43 -0700

    Update problem 23 to python 3

--- problem_0023.py
+++ problem_0023.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 """A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
 
 A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
@@ -8,25 +8,26 @@ As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest numb
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers."""
 
 from common import factors_of_num
+from time import time
 
 def main():
-	abundant_list = []
-	nums = range(28124)
-	for i in range(12,28123):
-		if is_abundant(i):
-			abundant_list.append(i)
-	for i in range(len(abundant_list)):
-		for j in range(i+1):
-			a = abundant_list[i]
-			b = abundant_list[j]
-			if a+b < len(nums):
-				nums[a+b] = 0
-	print sum(nums)
+    abundant_list = []
+    nums = list(range(28124))
+    for i in range(12,28123):
+        if is_abundant(i):
+            abundant_list.append(i)
+    for i in range(len(abundant_list)):
+        for j in range(i+1):
+            a = abundant_list[i]
+            b = abundant_list[j]
+            if a+b < len(nums):
+                nums[a+b] = 0
+    print('Answer: {}'.format(sum(nums)))
 
 def is_abundant(num):
-	if sum(factors_of_num(num)) > num:
-		return True
-	return False
+    return sum(factors_of_num(num)) > num
 
 if __name__ == "__main__":
-	main()
+    start = time()
+    main()
+    print('That took {:0.4f} seconds'.format(time() - start))
