526a4fa3945e20532eb809f95627a6805815aec1
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2016-10-24 22:07:50 -0700

    Updated problem 28 to python3

--- problem_0028.py
+++ problem_0028.py
@@ -1,30 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 """Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
 
-	21 22 23 24 25
-	20  7  8  9 10
-	19  6  1  2 11
-	18  5  4  3 12
-	17 16 15 14 13
+    21 22 23 24 25
+    20  7  8  9 10
+    19  6  1  2 11
+    18  5  4  3 12
+    17 16 15 14 13
 
-	It can be verified that the sum of the numbers on the diagonals is 101.
+    It can be verified that the sum of the numbers on the diagonals is 101.
 
-	What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?"""
+    What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?"""
 
-from common import counter
+from time import time
 
 def main():
-	print spiral(1001)
+    print('Answer: {}'.format(spiral(1001)))
 
 def spiral(n):
-	spiral_sum = 1
-	count = 1
-	for i in range((n-1)/2):
-		for j in range(4):
-			count += (i+1)*2
-#			print "Adding %d" % count
-			spiral_sum += count
-	return spiral_sum 
+    spiral_sum = 1
+    count = 1
+    for i in range((n-1)//2):
+        for _ in range(4):
+            count += (i+1)*2
+            spiral_sum += count
+    return spiral_sum
 
 if __name__ == "__main__":
-	main()
+    start = time()
+    main()
+    print('That took {:4.2f} seconds'.format(time() - start))
