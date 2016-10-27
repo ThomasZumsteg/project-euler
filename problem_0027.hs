76e04d15e09160752b14763b42c7d965c31eec67
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2016-10-26 18:45:14 -0700

    Start on problem 27 in haskell

---
+++ problem_0027.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
+
+-- Euler discovered the remarkable quadratic formula:
+-- n2+n+41
+-- It turns out that the formula will produce 40 primes for the consecutive integer values 0≤n≤39
+-- . However, when n=40,402+40+41=40(40+1)+41 is divisible by 41, and certainly when n=41,412+41+41
+-- is clearly divisible by 41.
+-- The incredible formula n2−79n+1601
+-- was discovered, which produces 80 primes for the consecutive values 0≤n≤79
+-- . The product of the coefficients, −79 and 1601, is −126479.
+-- Considering quadratics of the form:
+--     n2+an+b
+-- , where |a|<1000 and |b|≤1000
+-- where |n|
+-- is the modulus/absolute value of n
+-- e.g. |11|=11 and |−4|=4
+-- Find the product of the coefficients, a
+-- and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
+
+import Test.HUnit ((@=?), assertBool, runTestTT, Test(..))
+import Text.Printf (printf)
+import System.Console.CmdArgs
+import Data.Time (getCurrentTime, diffUTCTime)
+
+import Data.List (maximumBy)
+
+data EulerArgs = 
+    AdHoc { lower::Integer, upper::Integer }
+    | Euler 
+    | UnitTest
+    deriving (Show, Data, Typeable)
+
+data Coefficents = Coefficents { b::Integer, c::Integer }
+
+problem0027 = error "Not Implemented"
+
+isPrime :: Integer -> Bool
+isPrime n = all noRemainer [2..(floor $ sqrt $ fromIntegral n)]
+    where
+        noRemainer d = 0 /= rem n d
+
+isPrimeTest = [
+    assertBool "9 is not prime" (not $ isPrime 9),
+    assertBool "6 is not prime" (not $ isPrime 6),
+    assertBool "4 is not prime" (not $ isPrime 4),
+    assertBool "3 is prime" (isPrime 3),
+    assertBool "2 is prime" (isPrime 2)]    
+
+unitTests = map TestCase $
+    isPrimeTest
+
+exec :: EulerArgs -> IO ()
+exec AdHoc{..}= do
+    let answer = problem0027 lower upper
+    printf ("For coefficnets in [%d, %d] the greates consecutive primes is %d " ++
+        "using the equation n^2 + %d*n + %d") lower upper (b answer) (c answer)
+exec Euler = do
+    let answer = problem0027 (-1000) 1000
+    printf "Answer: %d\n" ((b answer) * (c answer))
+exec UnitTest = do 
+    runTestTT $ TestList unitTests
+    return ()
+
+main :: IO ()
+main = do
+    args <- cmdArgs $ modes [AdHoc{ lower = 1, upper = 10 }, Euler, UnitTest]
+    start <- getCurrentTime
+    exec args
+    stop <- getCurrentTime
+    print $ diffUTCTime stop start
