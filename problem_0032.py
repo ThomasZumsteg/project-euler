0dab0ddcdc3d29807e7ddcd558b730260ae0cd4f
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2016-11-07 20:31:26 -0800

    Update problem 32 to python3
    Also adds logging and timing

--- problem_0032.py
+++ problem_0032.py
@@ -1,38 +1,52 @@
-#!/usr/bin/python
-"""http://projecteuler.net/problem=32"""
+#!/usr/bin/env python3
 
+"""
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+"""
+
+from itertools import permutations
+import logging
 from re import sub
-from common import lexicographic_gen
+import sys
+from time import time
+
+logging.basicConfig(level=logging.WARNING, stream=sys.stderr,
+    format="[%(levelname)-8s] %(message)s")
 
 def main():
-	product_list = []
-	for order in lexicographic_gen('123456789'):
-		for i in range(1,len(order)-1):
-			for j in range(i+1,len(order)):
-#				print "%s x %s = %s" %(str((0,i)),str((i,j)),str((j,len(order))))
-#				print "%s x %s = %s" %(order[:i],order[i:j],order[j:])
-				a = int(order[:i])
-				b = int(order[i:j])
-				c = int(order[j:] )
-				product = a * b
-				if product > c:
-					break
-				if product == c:
-					print "%d x %d = %d" %(a,b,c)
-					product_list.append(product)
-	print sum(list(set(product_list)))
+    product_list = []
+    for perm in permutations('123456789'):
+        order = ''.join(perm)
+        logging.debug(order)
+        for i in range(1,len(order)-1):
+            for j in range(i+1,len(order)):
+                a = int(order[:i])
+                b = int(order[i:j])
+                c = int(order[j:] )
+                product = a * b
+                if product > c:
+                    break
+                if product == c:
+                    logging.info('{} x {} = {}'.format(a,b,c))
+                    product_list.append(product)
+    print(sum(list(set(product_list))))
 
 def pair_gen(digits):
-	pass
+    pass
 
 def is_pandigital(digits, n=9):
-	for i in range(1,n+1):
-		if not digits.count(i) == 1:
-			return False
-	for i in range(n+1,10):
-		if not digits.count(i) == 0:
-			return False
-	return True
+    for i in range(1,n+1):
+        if not digits.count(i) == 1:
+            return False
+    for i in range(n+1,10):
+        if not digits.count(i) == 0:
+            return False
+    return True
 
 if __name__ == "__main__":
-	main()
+    start = time()
+    main()
+    print('That took {:.4f} seconds'.format(time() - start))
