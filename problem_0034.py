bb222010c04f98083d02801efadbaf19057d293b
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2016-11-23 19:03:05 -0800

    Updated problem 34 to python3

--- problem_0034.py
+++ problem_0034.py
@@ -1,27 +1,34 @@
-#!/usr/bin/python
-"""http://projecteuler.net/problem=34"""
+#!/usr/bin/env python3
+"""
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+"""
+
+import time
 
 def main():
-	factorial_sum = 0
-	for i in range(3,2540161):
-		factorial = digit_fact(i)
-		if i == factorial:
-			factorial_sum += i
-			print i
-	print factorial_sum
+    factorial_sum = 0
+    for i in range(3,2540161):
+        factorial = digit_fact(i)
+        if i == factorial:
+            factorial_sum += i
+    print('Answer: {}'.format(factorial_sum))
 
 def digit_fact(num):
-	num = list(str(num))
-	num_sum = 0
-	for i in num:
-		num_sum += fact(int(i))
-	return num_sum
+    num = list(str(num))
+    num_sum = 0
+    for i in num:
+        num_sum += fact(int(i))
+    return num_sum
 
 def fact(num):
-	fact_sum = 1
-	for i in range(1,num+1):
-		fact_sum *= i
-	return fact_sum
+    fact_sum = 1
+    for i in range(1,num+1):
+        fact_sum *= i
+    return fact_sum
 
 if __name__ == "__main__":
-	main()
+    start = time.time()
+    main()
+    print('That took {:4.2f}'.format(time.time() - start))
