097d2f8599301fc7155ab781cf20cca699209503
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2016-12-02 10:25:10 -0800

    Update problem 38 to python3

--- problem_0038.py
+++ problem_0038.py
@@ -1,30 +1,40 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 """http://projecteuler.net/problem=38"""
 
+import time
+import logging
+import sys
 from common import counter
 
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stderr,
+    format='%(levelname)s: %(message)s')
+
 def main():
-	max_concat = 0
-	for i in range(1,10000):
-#		print "New %d" %i
-		concat_num = ''
-		for j in counter(1):
-			concat_num += str(i*j)
-#			print "Testing %s" % concat_num
-			if len(concat_num) > 9:
-#				print "Failed"
-				break
-			elif len(concat_num) == 9:
-				if is_pandigital(concat_num):
-					print "Found pandigital %d: %s" %(i, concat_num)
-					max_concat = max(max_concat, int(concat_num))
-	print max_concat
+    max_concat = 0
+    for i in range(1,10000):
+        logging.debug("New {}".format(i))
+        concat_num = ''
+        for j in counter(1):
+            concat_num += str(i*j)
+            logging.debug("Testing {}".format(concat_num))
+            if len(concat_num) > 9:
+                logging.debug("Failed")
+                break
+            elif len(concat_num) == 9:
+                if is_pandigital(concat_num):
+                    logging.info("Found pandigital {}: {}".format(i, concat_num))
+                    max_concat = max(max_concat, int(concat_num))
+    print('Answer: {}'.format(max_concat))
 
 def is_pandigital(string):
-	for i in range(1,10):
-		if string.count(str(i)) != 1:
-			return False
-	return True
+    for i in range(1,10):
+        if string.count(str(i)) != 1:
+            return False
+    return True
 
 if __name__ == "__main__":
-	main()
+    start = time.time()
+    main()
+    print('That took {:4.2f} seconds'.format(time.time() - start))
