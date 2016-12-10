3d9b2ced0b730b72818af5b36796474004c08c08
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2016-12-09 21:03:27 -0800

    Speed improvements to problem 39 in haskell

--- problem_0039.hs
+++ problem_0039.hs
@@ -12,45 +12,47 @@ import Data.Time (getCurrentTime, diffUTCTime)
 import Data.List (maximumBy)
 
 data EulerArgs = 
-    AdHoc { limit::Integer }
+    AdHoc { limit::Int }
     | Euler 
     | UnitTest
     deriving (Show, Data, Typeable)
 
 data Triple = Triple { 
-    a::Integer, 
-    b::Integer, 
-    c::Integer 
+    a::Int, 
+    b::Int, 
+    c::Int
     } deriving (Show, Eq)
 
-problem0038 :: Integer -> (Integer, [Triple])
-problem0038 lim = maximumBy sndLength $ map makeTuple [1..lim]
+problem0038 :: Int -> [Triple]
+problem0038 lim = maximumBy numItems $ map triples [1..lim]
     where
-        isRight (Triple a b c) = a^2 + b^2 == c^2
-        makeTuple p = (p, filter isRight $ triples p)
-        sndLength (_, x) (_, y) = compare (length x) (length y)
+        numItems x y = compare (length x) (length y)
 
-triples :: Integer -> [Triple]
+triples :: Int -> [Triple]
 triples p = [Triple a b c | 
-    c <- [(ceiling $ (fromIntegral p) / 3)..(p - 2)],
-    b <- [(ceiling $ (fromIntegral $ p - c) / 2)..(min c (p - c - 1))],
-    let a = p - c - b ]
+    c <- [(quot (p + 1) 3)..(p - 2)],
+    b <- [(quot (p - c + 1) 2)..(min c (p - c - 1))],
+    let a = p - c - b,
+    a^2 + b^2 == c^2 ]
 
 triplesTest = [
-    [Triple 2 2 2, Triple 1 2 3, Triple 1 1 4] @=? triples 6,
+    [Triple 30 40 50, Triple 24 45 51, Triple 20 48 52] @=? triples 120,
     True @=? (all (\t -> ( a t <= b t ) && ( b t <= c t )) $ triples 10)]
+
+perimeter :: Triple -> Int
+perimeter (Triple a b c) = a + b + c
 
 unitTests = map TestCase $
     triplesTest
 
 exec :: EulerArgs -> IO ()
 exec AdHoc{..}= do
-    let (perimeter, solutions) = problem0038 limit
+    let solutions = problem0038 limit
     printf "Less than %d the perimeter %d has %d unit solutions:\n" 
-        limit perimeter (length solutions)
+        limit (perimeter $ head solutions) (length solutions)
     mapM_ (\s -> printf "(%d, %d, %d)\n" (a s) (b s) (c s)) solutions
 exec Euler = do
-    let answer = fst $ problem0038 1000
+    let answer = perimeter $ head $ problem0038 1000
     printf "Answer: %d\n" answer
 exec UnitTest = do 
     runTestTT $ TestList unitTests
