ec6ff15efc00eb1bcc339f1419d0e393a666f7ff
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-01-05 16:38:30 -0800

    Updated problem 48 to python3

--- problem_0048.py
+++ problem_0048.py
@@ -1,13 +1,27 @@
-#!/usr/bin/python
-"""http://projecteuler.net/problem=48"""
+#!/usr/bin/env python3
+"""
+The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+"""
+
+import time
+import sys
+import logging
+
+logging.basicConfig(
+    stream=sys.stderr,
+    level=logging.INFO,
+    format='%(levelname)s:%(message)s')
 
 def main():
-	num_sum = 0
-	for num in reversed(range(1,1001)):
-		digits = num**num % int(1e10)
-		num_sum += digits
-	num_sum = num_sum % int(1e10)
-	print num_sum
+    num_sum = 0
+    for num in reversed(range(1,1001)):
+        digits = num**num % int(1e10)
+        num_sum += digits
+    num_sum = num_sum % int(1e10)
+    print('Answer: {}'.format(num_sum))
 
 if __name__ == "__main__":
-	main()
+    start = time.time()
+    main()
+    logging.info('That took {:4.2f} seconds'.format(time.time() - start))
