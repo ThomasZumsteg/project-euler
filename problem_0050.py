a1d03be5e4b7f19c6f924d375a5b4cf7c6468918
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-01-09 15:02:44 -0800

    Update problem 50 to python3

--- problem_0050.py
+++ problem_0050.py
@@ -1,75 +1,88 @@
-#!/usr/bin/python
-"""http://projecteuler.net/problem=50"""
+#!/usr/bin/env python3
+"""
+The prime 41, can be written as the sum of six consecutive primes:
+    41 = 2 + 3 + 5 + 7 + 11 + 13
+    This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+    The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+    Which prime, below one-million, can be written as the sum of the most consecutive primes?
+"""
 
-from time import time
+import logging
+import sys
+import time
+
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stderr,
+    format='%(levelname)s:%(message)s')
 
 def main():
-	block = 100000
-	start = time()
-	primes = prime_sieve(block)
-	i = 0
-	max_i = 0
-	max_prime_set = 0
-	while True:
-		i += 1
-		if i >= len(primes):
-			primes = prime_sieve(block,primes)
-		if primes[i] > 1000000: break
-#		print "Testing %d" %(primes[i])
-		prime_set = is_prime_sum(primes[i],primes, max_prime_set)
-		if prime_set and max_prime_set < len(prime_set):
-			max_prime_set = len(prime_set)
-			max_i = i
-#			print "%d is the sum of %d primes" %(primes[i], max_prime_set)
-	print "%d is the sum of %d primes" %(primes[max_i], max_prime_set)
-	print "That took %f seconds" %(time()-start)
+    block = 100000
+    primes = prime_sieve(block)
+    i = 0
+    max_i = 0
+    max_prime_set = 0
+    while True:
+        i += 1
+        if i >= len(primes):
+            primes = prime_sieve(block,primes)
+        if primes[i] > 1000000: break
+        logging.debug("Testing {}".format(primes[i]))
+        prime_set = is_prime_sum(primes[i],primes, max_prime_set)
+        if prime_set and max_prime_set < len(prime_set):
+            max_prime_set = len(prime_set)
+            max_i = i
+            logging.debug("{} is the sum of {} primes".format(
+                primes[i], max_prime_set))
+    print("{} is the sum of {} primes".format(primes[max_i], max_prime_set))
 
 def is_prime_sum(num,primes,sum_min):
-	i = 0
-	while True:
-		prime_sum = primes[i:i+sum_min]
-		if sum(prime_sum) > num: return False
-		j = i + sum_min 
-		while True:
-			prime_sum.append(primes[j])
-			if sum(prime_sum) > num: break
-			elif sum(prime_sum) == num: return prime_sum
-			j += 1
-		i += 1
+    i = 0
+    while True:
+        prime_sum = primes[i:i+sum_min]
+        if sum(prime_sum) > num: return False
+        j = i + sum_min
+        while True:
+            prime_sum.append(primes[j])
+            if sum(prime_sum) > num: break
+            elif sum(prime_sum) == num: return prime_sum
+            j += 1
+        i += 1
 
 def prime_sieve(extend,primes=[]):
-#	print "Called"
-	nums = [ True ] * (extend)
-	if not primes: offset = 2
-	else: 
-		offset = primes[-1]+1
-		for p in primes:
-			start = ( offset - 1 ) // p + 1
-			end   = ( offset + extend - 1 ) // p + 1
-			for n in range(start, end):
-#				print "%d is not prime (%d * %d)" %(n*p, n,p)
-				nums[n*p-offset] = False
-#				print "Marked %d False" % (n*p-offset)
-	for i in range(extend):
-		if nums[i]:
-#			print "Found %d" % (i+offset)
-			primes.append(i+offset)
-			for n in range(2,1+extend//(i+offset)):
-#				print "%d is not prime it %d * %d" %((i+offset)*n, n, i+offset)
-				nums[(i+offset)*n-offset] = False
-#	print nums
-	return primes
+    logging.debug("Called prime_sieve")
+    nums = [ True ] * (extend)
+    if not primes: offset = 2
+    else:
+        offset = primes[-1]+1
+        for p in primes:
+            start = ( offset - 1 ) // p + 1
+            end   = ( offset + extend - 1 ) // p + 1
+            for n in range(start, end):
+                logging.debug("{} is not prime ({} * {})".format(n*p, n,p))
+                nums[n*p-offset] = False
+                logging.debug("Marked {} False".format(n*p-offset))
+    for i in range(extend):
+        if nums[i]:
+            logging.debug("Found {}".format(i+offset))
+            primes.append(i+offset)
+            for n in range(2,1+extend//(i+offset)):
+                logging.debug("{} is not prime it {} * {}".format(
+                (i+offset)*n, n, i+offset))
+                nums[(i+offset)*n-offset] = False
+    logging.debug(nums)
+    return primes
 
 def prime_generator(block=100000):
-	primes = []
-	i = 0
-	while True:
-		if len(primes) <= i:
-			primes = prime_sieve(block, primes)
-		yield primes[i]
-		i += 1
+    primes = []
+    i = 0
+    while True:
+        if len(primes) <= i:
+            primes = prime_sieve(block, primes)
+        yield primes[i]
+        i += 1
 
 if __name__ == "__main__":
-	main()
-#	primes = prime_sieve(10000)
-#	print is_prime_sum(41,primes,2)
+    start = time.time()
+    main()
+    logging.info('That took {:4.2f} seconds'.format(time.time() - start))
