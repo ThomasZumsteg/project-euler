2651fda5bd0d417021a524aa6efe328a2cc5304f
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-01-16 20:42:47 -0800

    Start on problem 51 in haskell

---
+++ problem_0051.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
+
+import Test.HUnit ((@=?), assertBool, runTestTT, Test(..))
+import Text.Printf (printf)
+import System.Console.CmdArgs
+import Data.Time (getCurrentTime, diffUTCTime)
+
+
+data EulerArgs =
+    Euler
+    | AdHoc{ size::Integer }
+    | UnitTest
+    deriving (Show, Data, Typeable)
+
+-- By replacing the 1st digit of the 2-digit number *3, it turns out that six of the nine possible values: 13, 23, 43, 53, 73, and 83, are all prime.
+-- By replacing the 3rd and 4th digits of 56**3 with the same digit, this 5-digit number is the first example having seven primes among the ten generated numbers, yielding the family: 56003, 56113, 56333, 56443, 56663, 56773, and 56993. Consequently 56003, being the first member of this family, is the smallest prime with this property.
+-- Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit, is part of an eight prime value family.
+
+problem0051 :: Integer -> [Integer]
+problem0051 = error "Not Implemented"
+
+primes :: [Integer]
+primes = 2 : [n | n <- [3,5..], isPrime n]
+
+primesTest = [
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] @=? (take 10 primes),
+    [2, 3] @=? (take 2 primes),
+    2 @=? (head primes)]
+
+isPrime :: Integer -> Bool
+isPrime 0 = False
+isPrime 1 = False
+isPrime n = all noRemainer primes'
+    where
+        primes' = takeWhile (\p -> p * p <= n) primes
+        noRemainer d = 0 /= rem n d
+
+isPrimeTest = [
+    assertBool "9 is not prime" (not $ isPrime 9),
+    assertBool "6 is not prime" (not $ isPrime 6),
+    assertBool "4 is not prime" (not $ isPrime 4),
+    assertBool "0 is not prime" (not $ isPrime 0),
+    assertBool "3 is prime" (isPrime 3),
+    assertBool "2 is prime" (isPrime 2)]
+
+unitTests = map TestCase $
+    isPrimeTest
+
+exec :: EulerArgs -> IO ()
+exec Euler = do
+    let  answer = problem0051 8
+    printf "Answer: %d\n" (head answer)
+exec AdHoc{..} = do
+    let answer = problem0051 size
+    printf "Answer: %d\n" (head answer)
+    mapM_ print answer
+exec UnitTest = do
+    runTestTT $ TestList unitTests
+    return ()
+
+main :: IO ()
+main = do
+    args <- cmdArgs $ modes [
+        Euler,
+        AdHoc{ size=1000 },
+        UnitTest]
+    start <- getCurrentTime
+    exec args
+    stop <- getCurrentTime
+    printf "That took %s\n"  $ show $ diffUTCTime stop start
