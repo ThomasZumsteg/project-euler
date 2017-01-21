b6b11799e93b85f6becc459419534e7fc967b03b
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-01-20 17:08:51 -0800

    Update problem 52 to python 3

--- problem_0052.py
+++ problem_0052.py
@@ -1,21 +1,31 @@
-#!/usr/bin/python
-"""http://projecteuler.net/problem=52"""
+#!/usr/bin/env python3
+"""
+It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+"""
 
 from time import time
+import sys
+import logging
 from common import counter
 
+logging.basicConfig(
+    stream=sys.stderr,
+    level=logging.INFO,
+    format='%(levelname)s:%(message)s')
+
 def main():
-	for i in counter(1):
-		digits = sorted(list(str(i)))
-		done = True
-		for j in range(2,7):
-			if not digits == sorted(list(str(i*j))):
-				done = False
-				break
-		if done: break
-	print i
+    for i in counter(1):
+        digits = sorted(list(str(i)))
+        done = True
+        for j in range(2,7):
+            if not digits == sorted(list(str(i*j))):
+                done = False
+                break
+        if done: break
+    print('Answer: {}'.format(i))
 
 if __name__ == "__main__":
-	start = time()
-	main()
-	print "That took %f seconds" %(time() - start)
+    start = time()
+    main()
+    logging.info("That took {:4.2f} seconds".format(time() - start))
