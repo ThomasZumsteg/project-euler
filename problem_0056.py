f027c7ff27c0790e690d669cb6f50ca30866f2b9
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-02-05 17:57:37 -0800

    Update problem 56 to python 3

--- problem_0056.py
+++ problem_0056.py
@@ -1,22 +1,34 @@
-#!/usr/bin/python
-"""http://projecteuler.net/problem=56"""
+#!/usr/bin/env python3
+"""
+A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+"""
 
-from time import time
+import logging
+import sys
+import time
+
+logging.basicConfig(
+    stream=sys.stderr,
+    level=logging.INFO,
+    format='%(levelname)s: %(message)s')
 
 def main():
-	max_digit_sum = 0
-	for a in range(100):
-		for b in range(100):
-			max_digit_sum = max(max_digit_sum, digit_sum(a**b))
-	print "Largest digit sum is %d" % max_digit_sum
+    max_digit_sum = 0
+    for a in range(100):
+        for b in range(100):
+            max_digit_sum = max(max_digit_sum, digit_sum(a**b))
+    print("Largest digit sum is {max_digit}"
+          .format(max_digit=max_digit_sum))
 
 def digit_sum(num):
-	sum_digits = 0
-	for c in str(num):
-		sum_digits += int(c)
-	return sum_digits
+    sum_digits = 0
+    for c in str(num):
+        sum_digits += int(c)
+    return sum_digits
 
 if __name__ == "__main__":
-	start = time()
-	main()
-	print "That took %f seconds" %(time() - start)
+    start = time.time()
+    main()
+    logging.info("That took {time:4.2f} seconds"
+                 .format(time=time.time() - start))
