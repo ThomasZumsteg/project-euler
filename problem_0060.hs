53fffe8c61e5aaee707b23592e8b46670887fa1d
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-02-26 21:16:25 -0800

    Clean up for classes in problem 60 haskell

--- problem_0060.hs
+++ problem_0060.hs
@@ -15,121 +15,64 @@ import Common (exec, EulerArg, euler_main, primes, isPrime)
 
 class Heap h where
     singleton :: a -> h a
-    remove :: h a -> Maybe (a, h a)
-    merge :: h a -> h a -> h a
-    insert :: h a -> a -> h a
+    remove :: (Ord a) => h a -> Maybe (a, h a)
+    merge :: (Ord a) => h a -> h a -> h a
+
+    insert :: (Ord a) => h a -> a -> h a
     insert h v = merge h (singleton v)
-    toList :: h a -> [a]
+
+    toList :: (Ord a) => h a -> [a]
     toList heap = case remove heap of
         Just (x, heap') -> x : toList heap'
         otherwise -> []
-    fromList :: [a] -> h a
+
+    fromList :: (Ord a) => [a] -> h a
     fromList (x:[]) = singleton x
     fromList (x:xs) = merge (singleton x) (fromList xs)
 
-data BHeap a = Empty | Leaf { 
-    value :: a, 
-    _nodes :: Int,
-    left :: BHeap a,
-    right :: BHeap a }
-    deriving Eq
+data BHeap a = Empty | Leaf {
+    value :: a,
+    _nodes :: Integer,
+    left  :: BHeap a,
+    right :: BHeap a
+} deriving Eq
 
-instance (Show a) => Show (BHeap a) where 
+instance (Show a) => Show (BHeap a) where
     show Empty = "Empty"
-    show (Leaf v d lh rh) = "(Leaf " ++ show v ++ " " ++ show d ++ 
-        " " ++ show lh ++ " " ++ show rh ++ ")"
+    show (Leaf v n l r) = "(" ++ show v ++ " " ++ show n ++ " " ++
+        show l ++ " " ++ show r ++ ")"
 
-instance (Ord a) => Heap (BHeap a) where
-    remove Empty = Nothing 
-    remove (Leaf v _ l r) = Just (v, merge l r)
+instance Heap BHeap where
     singleton v = Leaf v 1 Empty Empty
-    merge = bMerge
-
-bMerge :: (Ord a) => BHeap a -> BHeap a -> BHeap a
-bMerge Empty h2 = h2
-bMerge h1 Empty = h1
-bMerge h1@(Leaf v1 n1 l1 r1) h2@(Leaf v2 n2 l2 r2) 
-    | v2 < v1  = bMerge (Leaf v2 n1 l1 r1) (Leaf v1 n2 l2 r2)
-    | (v2 == v1 && n1 < n2) = bMerge h2 h1
-    | nodes l1 > nodes r1 = h' l1 (bMerge r1 h2)
-    | otherwise = h' (bMerge l1 h2) r1
+    remove Empty = Nothing
+    remove (Leaf v _ l r) = Just (v, merge l r)
+    merge Empty h2 = h2
+    merge h1 Empty = h1
+    merge h1@(Leaf v1 n1 l1 r1) h2@(Leaf v2 n2 _ _)
+        | v2 < v1 = merge h2 h1
+        | v2 == v1 && n1 < n2 = merge h2 h1
+        | nodes r1 < nodes l1 = Leaf v1 (n1 + n2) r1 (merge l1 h2)
+        | otherwise = Leaf v1 (n1 + n2) (merge r1 h2) l1
         where
-            h' = Leaf v1 (n1 + n2)
             nodes Empty = 0
             nodes h = _nodes h
 
-
-bMergeTest = [
-    (Leaf 1 2 (Leaf 2 1 Empty Empty) Empty) @=?  bMerge 
-    (Leaf 2 1 Empty Empty) 
-    (Leaf 1 1 Empty Empty)]
-
-insertTest = [
-    (Leaf 1 3 (Leaf 3 1 Empty Empty) (Leaf 2 1 Empty Empty)) @=?  
-        insert (Leaf 2 2 (Leaf 3 1 Empty Empty) Empty) 1,
-    (Leaf 1 2 (Leaf 3 1 Empty Empty) Empty) @=? insert (Leaf 3 1 Empty Empty) 1,
-    [1] @=? (toList $ insert Empty 1),
-    [1, 2] @=? (toList $ insert (Leaf 2 1 Empty Empty) 1),
-    [1, 2] @=? (toList $ insert (Leaf 1 1 Empty Empty) 2),
-    [2] @=? (toList $ insert Empty 2)]
-
-fromListTest = [
-    [1,2,3] @=? (toList $ fromList [3,2,1]::[Integer]),
-    ([1,2,3]::Integer) @=? (toList $ fromList [1,3,2]),
-    (Leaf 1 3 (Leaf 2 1 Empty Empty) (Leaf 3 1 Empty Empty)) @=? fromList [1,2,3]]
+bHeapTest = [
+    (Leaf 1 1 Empty Empty) @=? singleton 1,
+    (Leaf 0 1 Empty Empty) @=? singleton 0,
+    "(1 1 Empty Empty)" @=? (show $ (singleton 1::BHeap Integer)),
+    "(0 1 Empty Empty)" @=? (show $ (singleton 0::BHeap Integer)),
+    (Leaf 0 2 (Leaf 1 1 Empty Empty) Empty) @=? merge (singleton 1) (singleton 0),   
+    (Leaf 0 2 (Leaf 1 1 Empty Empty) Empty) @=? merge (singleton 0) (singleton 1),
+    (Leaf 0 2 (Leaf 1 1 Empty Empty) Empty) @=? insert (singleton 0) 1,
+    (Leaf 0 2 (Leaf 1 1 Empty Empty) Empty) @=? insert (singleton 1) 0]
+    
 
 problem0060 :: Int -> [Set.Set Integer]
-problem0060 n = filter ((==n) . length) primeSets
+problem0060 = error "Not Implemented"
 
-concatPairs :: Set.Set Integer -> Set.Set Integer
-concatPairs = Set.map read' . combinations . show'
-    where
-        read' (a, b) = read (a ++ b)
-        show' = Set.map show
-
-concatPairsTest = [ 
-    (Set.fromList [12,21]) @=? (concatPairs $ Set.fromList [1,2]),
-    (Set.fromList [12,21,13,31,23,32]) @=? (concatPairs $ Set.fromList [1,2,3])]
-
-combinations :: (Ord a) => Set.Set a -> Set.Set (a, a)
-combinations s = Set.fromList [(n, m) | n <- ls, m <- ls, m /= n]
-    where
-        ls = Set.toList s
-
-combinationsTest = [
-    (Set.fromList []) @=? (combinations $ Set.fromList ""),
-    (Set.fromList [('a','b'), ('b','a')]) @=? (combinations $ Set.fromList "ab")]
-
-primeSets :: [Set.Set Integer]
-primeSets = map Set.fromList [ [l] | l <- [0..]]
-
-primeSetsTest = [
-    Set.fromList [] @=? primeSets !! 0,
-    Set.fromList [2] @=? primeSets !! 1,
-    Set.fromList [3] @=? primeSets !! 2,
-    Set.fromList [2,3] @=? primeSets !! 3,
-    Set.fromList [5] @=? primeSets !! 4,
-    Set.fromList [2,5] @=? primeSets !! 5,
-    Set.fromList [7] @=? primeSets !! 6,
-    Set.fromList [3,5] @=? primeSets !! 7,
-    Set.fromList [2,3,5] @=? primeSets !! 8]
-
-nCombinations :: Int -> [a] -> [[a]]
-nCombinations 0 _ = [[]]
-nCombinations _ [] = []
-nCombinations i (x:xs) = (map (x:) $ nCombinations (i-1) xs) ++ (nCombinations i xs)
-
-nCombinationsTest = [
-    [] @=? (take 10 $ nCombinations 2 [1..]),
-    ["ab","ac","bc"] @=? nCombinations 2 "abc",
-    ["ab"] @=? nCombinations 2 "ab",
-    ["a", "b"] @=? nCombinations 1 "ab",
-    ["a"] @=? nCombinations 1 "a",
-    [] @=? nCombinations 1 ""]
-
-unitTests = map TestCase $
-    bMergeTest ++
-    fromListTest
+unitTests = map TestCase
+    bHeapTest
 
 data Arg = Euler | AdHoc { limit::Double } | UnitTest
     deriving (Show, Data, Typeable)
