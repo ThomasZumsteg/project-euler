7f6bd5a3a965f39c673a528f8f71f589ea6e5db2
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-03-06 21:30:04 -0800

    Another start at problem 60 in haskell

--- problem_0060.hs
+++ problem_0060.hs
@@ -13,21 +13,25 @@ import Common (exec, EulerArg, euler_main, primes, isPrime)
 -- The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
 -- Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
 
-data ListHeap a = 
-    Nil | List [a] (ListHeap a) (ListHeap a)
-        deriving (Show, Eq)
+class Heap h where
+    singleton :: a -> h
+    remove :: h -> Maybe (a, h)
 
-instance (Ord a, Num a) => Ord (ListHeap a) where
-    compare Nil Nil = EQ
-    compare _ Nil = LT
-    compare Nil _ = GT
-    compare (List l1 _ _) (List l2 _ _) = compare (sum l1) (sum l2)
+data ListHeap a = Nil | Value a | List [ListHeap a]
 
-problem0060 :: Int -> [Integer]
-problem0060 n = head $ filter (\s -> len n s && (all property $ pairs s)) $ toList primeSets
-    where
-        len l ns = l == length ns
-        pairs xs = [(n, m) | n <- xs, m <- xs, m /= n]
+problem0060 :: Int -> [[Integer]]
+problem0060 = filter (all property . pairs) . toList . setsOfLength primes
+
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs (x:xs) = [(x, y) | y <- xs] ++ pairs xs
+
+pairsTest = [
+    [] @=? pairs "",
+    [] @=? pairs [1],
+    [(1,2)] @=? pairs [1,2],
+    [(1,2),(1,3),(2,3)] @=? pairs [1,2,3]
+    ]
 
 property :: (Integer, Integer) -> Bool
 property (p, q) = pqIsPrime && qpIsPrime
@@ -37,75 +41,37 @@ property (p, q) = pqIsPrime && qpIsPrime
         qpIsPrime = isPrime $ read $ p' ++ q'
         pqIsPrime = isPrime $ read $ q' ++ p'
 
-buildHeap :: [a] -> [a] -> ListHeap a
-buildHeap [] [] = Nil
-buildHeap root [] = Nil
-buildHeap root (x:xs) = List (x:root) without with
-    where
-        without = buildHeap root xs 
-        with = buildHeap (x:root) xs 
-
-buildHeapTest = [
-    (List [1] (List [2] Nil Nil) (List [2,1] Nil Nil)) @=? buildHeap [] [1,2],
-    (List [1] Nil Nil) @=? buildHeap [] [1],
-    Nil @=? buildHeap [1] [],
-    (Nil::ListHeap Int) @=? buildHeap [] []
+propertyTest = [
+    True @=? property (3, 7),
+    True @=? property (3, 109),
+    True @=? property (109, 3),
+    False @=? property (101, 3)
     ]
 
-merge:: (Ord a, Num a) => ListHeap a -> ListHeap a -> ListHeap a
-merge Nil Nil = Nil
-merge h1 Nil = h1
-merge Nil h2 = h2
-merge h1@(List v1 l1 r1) h2@(List v2 _ _)
-    | h1 > h2 = merge h2 h1
-    | l1 == Nil = List v1 h2 r1
-    | r1 == Nil = List v1 l1 h2
-    | otherwise = List v1 l1 (merge h2 r1)
+combinations :: Int -> [a] -> ListHeap a
+combinations = error "Not Implemeneted"
 
-mergeTest = [
-    Nil @=? merge Nil Nil,
-    (List [1] Nil Nil) @=? merge Nil (List [1] Nil Nil),
-    (List [1] Nil Nil) @=? merge (List [1] Nil Nil) Nil,
-    (List [1] (List [2] Nil Nil) Nil) @=? merge (List [1] Nil Nil) (List [2] Nil Nil)
+combinationsTest = [
     ]
 
-toList :: (Ord a, Num a) => ListHeap a -> [[a]]
-toList Nil = []
-toList (List i l r) = i : (toList $ merge l r)
+toList :: ListHeap a -> [a]
+toList = error "Not Implemented"
 
 toListTest = [
-    [1] @=? ((!!0) $ toList $ buildHeap [] [1..]),
-    [2] @=? ((!!1) $ toList $ buildHeap [] [1..]),
-    [3] @=? ((!!2) $ toList $ buildHeap [] [1..]),
-    [2,1] @=? ((!!3) $ toList $ buildHeap [] [1..]),
-    [3,1] @=? ((!!4) $ toList $ buildHeap [] [1..]),
-    [4] @=? ((!!5) $ toList $ buildHeap [] [1..]),
-    [5] @=? ((!!6) $ toList $ buildHeap [] [1..]),
-    ([[1],[2],[3],[2,1],[3,1],[3,2],[3,2,1]]::[[Integer]]) @=? (toList $ buildHeap [] [1,2,3])
     ]
 
-primeSets :: ListHeap Integer
-primeSets = buildHeap [] primes
+setsOfLength :: [a] -> Int -> ListHeap [a]
+setsOfLength = error "Not Implemented"
 
-primeSetsTest = [
-    [2] @=? (toList primeSets) !! 0,
-    [3] @=? (toList primeSets) !! 1,
-    [5] @=? (toList primeSets) !! 2,
-    [3,2] @=? (toList primeSets) !! 3,
-    [5,2] @=? (toList primeSets) !! 4,
-    [7] @=? (toList primeSets) !! 5,
-    [5,3] @=? (toList primeSets) !! 6,
-    [7,2] @=? (toList primeSets) !! 7,
-    [7,3] @=? (toList primeSets) !! 8,
-    [5,3,2] @=? (toList primeSets) !! 9,
-    [11] @=? (toList primeSets) !! 10
+setsOfLengthTest = [
     ]
 
 unitTests = map TestCase $
-    buildHeapTest ++
-    mergeTest ++
+    setsOfLengthTest ++
     toListTest ++
-    primeSetsTest
+    combinationsTest ++
+    propertyTest ++ 
+    pairsTest
 
 data Arg = Euler | AdHoc { limit::Double } | UnitTest
     deriving (Show, Data, Typeable)
