6d36b6014dc0ac9d01c1501c6cecbb171a128c47
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-03-10 23:44:50 -0800

    More work on problem 60 in haskell

--- problem_0060.hs
+++ problem_0060.hs
@@ -6,109 +6,51 @@ import Text.Printf (printf)
 import System.Console.CmdArgs
 import Data.List (sort)
 
-import Data.Maybe (fromJust)
-
 import Common (exec, EulerArg, euler_main, primes, isPrime)
 
 -- The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
 -- Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
 
+problem0060 :: Int -> [Set.Set Integer]
+problem0060 n = error "Not Implemented"
 
-data ListHeap a = Nil | Value a | List [ListHeap a]
-
-instance Heap ListHeap where
-    singleton v = Value v
-    remove Nil = Nothing
-    remove (Value v) = Just (v, Nil)
-    remove (List []) = Nothing
-    remove (List (h:hs)) = case remove h of
-        Just (v, h') -> Just (v, (merge h' (List hs)))
-        otherwise -> Nothing
-    merge h1 h2 = error "Not Implemented"
-
-instance (Ord a) => Eq (ListHeap a) where
-    (==) h1 h2 = EQ == compare h1 h2
-
-instance (Ord a, Eq a) => Ord (ListHeap a) where
-    compare Nil Nil = EQ
-    compare Nil _ = GT
-    compare _ Nil = LT
-    compare (Value v1) (Value v2) = compare v1 v2
-    compare (List (x:xs)) (List (y:ys)) = if x == y then
-        compare (List xs) (List ys) else
-        compare x y
-
-problem0060 :: Int -> [[Integer]]
-problem0060 = filter (all property . pairs) . toList . setsOfLength primes
-
-pairs :: [a] -> [(a, a)]
-pairs [] = []
-pairs (x:xs) = [(x, y) | y <- xs] ++ pairs xs
-
-pairsTest = [
-    [] @=? pairs "",
-    [] @=? pairs [1],
-    [(1,2)] @=? pairs [1,2],
-    [(1,2),(1,3),(2,3)] @=? pairs [1,2,3]
-    ]
-
-property :: (Integer, Integer) -> Bool
-property (p, q) = pqIsPrime && qpIsPrime
-    where
-        p' = show p
-        q' = show q
-        qpIsPrime = isPrime $ read $ p' ++ q'
-        pqIsPrime = isPrime $ read $ q' ++ p'
+property :: Integer -> Integer -> Bool
+property a b = isPrime $ read (show a ++ show b)
 
 propertyTest = [
-    True @=? property (3, 7),
-    True @=? property (3, 109),
-    True @=? property (109, 3),
-    False @=? property (101, 3)
+    True @=? property 3 7,
+    True @=? property 7 3,
+    False @=? property 11 7
     ]
 
-combinations :: Int -> [a] -> ListHeap a
-combinations = error "Not Implemeneted"
+nLenSets :: Int -> [a] -> [[a]]
+nLenSets 0 _ = [[]]
+nLenSets _ [] = []
+nLenSets n (x:xs) = with_x ++ without_x
+    where
+        with_x = map (x:) $ nLenSets (n-1) xs
+        without_x = nLenSets n xs
 
-combinationsTest = [
+deepSort = sort . map sort
+
+nLenSetsTest = [
+    [[1,2],[1,3],[2,3]] @=? (deepSort $ nLenSets 2 [1,3,2]),
+    [[1,2],[1,3],[2,3]] @=? (deepSort $ nLenSets 2 [1,2,3]),
+    [[1,2]] @=? (deepSort $ nLenSets 2 [1,2]),
+    [[1],[2]] @=? (deepSort $ nLenSets 1 [1,2])
     ]
 
-toList :: ListHeap a -> [a]
-toList h = case remove h of
-    Just (v, h') -> v : toList h'
-    otherwise -> []
-
-toListTest = [
-    "" @=? toList Nil,
-    [1] @=? toList (Value 1),
-    [1,2] @=? toList (List [Value 1, Value 2]),
-    [1,2,3,4,5] @=? toList (
-        List [
-            List [Value o | o <- [1,3..10]],
-            List [Value e | e <- [2,4..10]]
-        ]
-    )]
-
-setsOfLength :: [a] -> Int -> ListHeap [a]
-setsOfLength = error "Not Implemented"
-
-setsOfLengthTest = [
-    ]
-
-unitTests = map TestCase $
-    setsOfLengthTest ++
-    toListTest ++
-    combinationsTest ++
-    propertyTest ++ 
-    pairsTest
+unitTests = map TestCase $ 
+    propertyTest ++
+    nLenSetsTest
 
 data Arg = Euler | AdHoc { limit::Double } | UnitTest
     deriving (Show, Data, Typeable)
 
 instance EulerArg Arg where
     exec Euler = do
-        let answer = head $ problem0060 5
-        printf "Answer: %s\n" (show answer)
+        let answer = sum $ Set.toList $ head $ problem0060 5
+        printf "Answer: %s\n" answer
     exec UnitTest = do
         runTestTT $ TestList unitTests
         return ()
