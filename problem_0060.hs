ebfb8ae2197323247848c0346e47a4a3a2a869b2
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-03-11 22:56:42 -0800

    Adding set functions

--- problem_0060.hs
+++ problem_0060.hs
@@ -12,7 +12,7 @@ import Common (exec, EulerArg, euler_main, primes, isPrime)
 -- Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
 
 problem0060 :: Int -> [Set.Set Integer]
-problem0060 n = error "Not Implemented"
+problem0060 n = Set.toAscList $ nLenSets n primes
 
 property :: Integer -> Integer -> Bool
 property a b = isPrime $ read (show a ++ show b)
@@ -23,21 +23,21 @@ propertyTest = [
     False @=? property 11 7
     ]
 
-nLenSets :: Int -> [a] -> [[a]]
-nLenSets 0 _ = [[]]
-nLenSets _ [] = []
-nLenSets n (x:xs) = with_x ++ without_x
+nLenSets :: (Ord a) => Int -> [a] -> Set.Set(Set.Set a)
+nLenSets 0 _ = Set.singleton Set.empty
+nLenSets _ [] = Set.empty
+nLenSets n (x:xs) = Set.union with_x without_x
     where
-        with_x = map (x:) $ nLenSets (n-1) xs
+        with_x = Set.map (Set.insert x) $ nLenSets (n-1) xs
         without_x = nLenSets n xs
 
-deepSort = sort . map sort
+fromLists = Set.fromList . map Set.fromList
 
 nLenSetsTest = [
-    [[1,2],[1,3],[2,3]] @=? (deepSort $ nLenSets 2 [1,3,2]),
-    [[1,2],[1,3],[2,3]] @=? (deepSort $ nLenSets 2 [1,2,3]),
-    [[1,2]] @=? (deepSort $ nLenSets 2 [1,2]),
-    [[1],[2]] @=? (deepSort $ nLenSets 1 [1,2])
+    (fromLists [[1,2],[1,3],[2,3]]) @=? nLenSets 2 [1,3,2],
+    (fromLists [[1,2],[1,3],[2,3]]) @=? nLenSets 2 [1,2,3],
+    (fromLists [[1,2]]) @=? nLenSets 2 [1,2],
+    (fromLists [[1],[2]]) @=? nLenSets 1 [1,2]
     ]
 
 unitTests = map TestCase $ 
@@ -50,7 +50,7 @@ data Arg = Euler | AdHoc { limit::Double } | UnitTest
 instance EulerArg Arg where
     exec Euler = do
         let answer = sum $ Set.toList $ head $ problem0060 5
-        printf "Answer: %s\n" answer
+        printf "Answer: %s\n" (show answer)
     exec UnitTest = do
         runTestTT $ TestList unitTests
         return ()
