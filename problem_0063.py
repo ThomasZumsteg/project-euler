ff8788b53bf90218ffaeff242b61db63ccf5c8ab
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-04-03 21:41:33 -0700

    Updated problem 63 to python 3

--- problem_0063.py
+++ problem_0063.py
@@ -1,23 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-from time import time
-from common import counter
+"""
+The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit number, 134217728=8^9, is a ninth power.
+How many n-digit positive integers exist which are also an nth power?
+"""
+
+import logging
+import sys
+import time
+from itertools import count
+
+logging.basicConfig(
+    stream=sys.stderr,
+    level=logging.INFO,
+    format='%(levelname)s: %(message)s')
 
 def main():
-	count = 0
-	for n in counter(1):
-		for m in reversed(range(1,10)):
-			num_digits = len(str(m**n))
-			if num_digits == n:
-#				ps = "%d = %d^%d"
-#				print ps %(m**n, m,n)
-				count += 1
-			elif num_digits < n: break
-		if m >= 9: break
-	ps = "There are %d numbers for which the nth power has n digits"
-	print ps %(count)
+    matches = 0
+    for n in count(1):
+        for m in reversed(range(1,10)):
+            num_digits = len(str(m**n))
+            if num_digits == n:
+                logging.debug( "{} = {}^{}".format(m**n, m,n))
+                matches += 1
+            elif num_digits < n: break
+        if m >= 9: break
+    ps = "There are {} numbers for which the nth power has n digits"
+    print(ps.format(matches))
 
 if __name__ == "__main__":
-	start = time()
-	main()
-	print "That took %f seconds" %(time() - start)
+    start = time.time()
+    main()
+    logging.info("That took {:4.2f} seconds".format(time.time() - start))
