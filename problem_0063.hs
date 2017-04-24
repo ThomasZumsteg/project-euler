8e7e00dc32ffe774e833aee2a7ef11d0d7ef721c
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-04-23 17:11:45 -0700

    Fixed bug in problem 63 in haskell

--- problem_0063.hs
+++ problem_0063.hs
@@ -9,20 +9,20 @@ import Common (exec, EulerArg, euler_main)
 -- The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit number, 134217728=8^9, is a ninth power.
 -- How many n-digit positive integers exist which are also an nth power?
 
-problem0063 :: Int
-problem0063 = length $ concat $ takeWhile (/=[]) $ map digitPower [1..]
+problem0063 :: [[Integer]]
+problem0063 = takeWhile (/=[]) $ map digitPower [1..]
 
 digits :: Integer -> Int
-digits n = floor (log (fromIntegral n) / log 10) + 1
+digits n = length $ show n
 
-slice :: (a -> Bool) -> [a] -> ([a],[a],[a])
+slice :: (Eq a) => (a -> Bool) -> [a] -> ([a],[a],[a])
 slice test xs = (drops, pass, past)
     where
         (drops, xs') = span (not . test) xs
         (pass, past) = span test xs'
 
 digitPower :: Integer -> [Integer]
-digitPower d = second $ slice digitEqualPower [1..]
+digitPower d = second $ slice digitEqualPower [1..10]
     where
         second (_, m, _) = m
         power = flip (^) d
@@ -31,7 +31,8 @@ digitPower d = second $ slice digitEqualPower [1..]
 testDigitPower = [
     [1..9] @=? digitPower 1,
     [4..9] @=? digitPower 2,
-    [5..10] @=? digitPower 3 
+    [5..9] @=? digitPower 3,
+    [] @=? digitPower 22
     ]
 
 unitTests = map TestCase $
@@ -43,14 +44,14 @@ data Arg = Euler | UnitTest |
 
 instance EulerArg Arg where
     exec Euler = do
-        let answer = problem0063
-        printf "Answer %d" answer 
+        let answer = length $ concat $ takeWhile (/=[]) problem0063
+        printf "Answer %d\n" answer 
     exec AdHoc{..} = do
-        let answer = concatMap digitPower [1..]
-        mapM_ (printf "%s\n" . show) answer
+        let answer = map (\n -> (n, digitPower n, map (flip (^) n) $ digitPower n)) [start..stop]
+        mapM_ (\(n, num, ans) -> printf "%d: %s %s\n" n (show num) (show ans)) answer
     exec UnitTest = do
         runTestTT $ TestList unitTests
         return ()
 
 main :: IO ()
-main = euler_main [Euler, UnitTest, AdHoc {start=5 , stop=9}]
+main = euler_main [Euler, UnitTest, AdHoc {start=1 , stop=25}]
