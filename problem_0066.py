4de393f5d442f870656ae8446b0495beef787c91
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-05-11 18:58:56 -0700

    Wrong destription for 66 in python

--- problem_0066.py
+++ problem_0066.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 """
-The square root of 2 can be written as an infinite continued fraction.
-√2 = 1 + (1/(2 + (1/(2 + (1/(2 + (1/(2 + ...))))))))
-The infinite continued fraction can be written, √2 = [1;(2)], (2) indicates that 2 repeats ad infinitum. In a similar way, √23 = [4;(1,3,1,8)].
-It turns out that the sequence of partial values of continued fractions for square roots provide the best rational approximations. Let us consider the convergents for √2.
-1 + (1/2) = (3/2)
-1 + (1/(2+(1/2))) = (7/5)
-1 + (1/(2+(1/(2+(1/2))))) = 17/12
-1 + (1/(2+(1/(2+(1/(2+(1/2))))))) = 41/29
-Hence the sequence of the first ten convergents for √2 are:
-1, 3/2, 7/5, 17/12, 41/29, 99/70, 239/169, 577/408, 1393/985, 3363/2378, ...
-What is most surprising is that the important mathematical constant,
-e = [2; 1,2,1, 1,4,1, 1,6,1 , ... , 1,2k,1, ...].
-The first ten terms in the sequence of convergents for e are:
-2, 3, 8/3, 11/4, 19/7, 87/32, 106/39, 193/71, 1264/465, 1457/536, ...
-The sum of digits in the numerator of the 10th convergent is 1+4+5+7=17.
-Find the sum of digits in the numerator of the 100th convergent of the continued fraction for e.
+Consider quadratic Diophantine equations of the form:
+x² – Dy² = 1
+For example, when D=13, the minimal solution in x is 649² – 13×180² = 1.
+It can be assumed that there are no solutions in positive integers when D is square.
+By finding minimal solutions in x for D = {2, 3, 5, 6, 7}, we obtain the following:
+3² – 2×2² = 1
+2² – 3×1² = 1
+9² – 5×4² = 1
+5² – 6×2² = 1
+8² – 7×3² = 1
+Hence, by considering minimal solutions in x for D ≤ 7, the largest x is obtained when D=5.
+Find the value of D ≤ 1000 in minimal solutions of x for which the largest value of x is obtained.
+
+(x, d, y)
+x² - Dy² = 1
+(x² - 1) / D = y²
+(x² - 1) = D q²
+(x² - 1) is a multiple of D and a square
 """
 
 import collections
