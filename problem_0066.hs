a6f4907185123abd5950558a2850dbc98d1fad15
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-05-18 21:21:50 -0700

    Cutting over to fraction expantion for eercise 66

--- problem_0066.hs
+++ problem_0066.hs
@@ -28,46 +28,64 @@ import qualified Data.Map.Lazy as Map
 -- (x² - 1) = D q²
 -- (x² - 1) is a multiple of D and a square
 problem0066 :: Integer -> Integer -> [(Integer, Integer, Integer)]
-problem0066 start stop = filter (\(_, d, _) -> (start <= d) && (d <= stop)) solutions
-
--- x² - Dy² = 1
--- (x² - 1) / y² = D
-diophantineSolutions :: Integer -> [(Integer, Integer)]
-diophantineSolutions v = map firstAndThird $  filter ((==v) . snd) solutions
+problem0066 start stop = [(x, d, y) |
+    d <- [start..stop],
+    not $ elem d squares,
+    let (x, y) = (0,0)]
     where
-        firstAndThird (i, _, k) = (i, k)
-        snd (_, j, _) = j
+        squares = [n*n | n <- [1..stop]]
 
-solutions :: [(Integer, Integer, Integer)]
-solutions = [(x,d,y) | 
-    x <- [2..], y <- [2..x],
-    let (d,r) = divMod (x*x-1) (y*y), r == 0]
+sumFractionExpantion :: (Integer, [Integer]) -> (Integer, Integer)
+sumFractionExpantion (x, xs) = (\(n, d) -> (x * d + n, n)) (foldr worker (0,0) xs)
+    where
+        worker n (m, e) = (e * n + m, m)
 
-diophantineSolutionsTest = [
-    (3,2) @=? (head $ diophantineSolutions 2),
-    (7,4) @=? (head $ diophantineSolutions 3),
-    (9,4) @=? (head $ diophantineSolutions 5),
-    (5,2) @=? (head $ diophantineSolutions 6 ),
-    (8,3) @=? (head $ diophantineSolutions 7 ),
-    (17,6) @=? (head $ diophantineSolutions 8 ),
-    (19,6) @=? (head $ diophantineSolutions 10),
-    (10,3) @=? (head $ diophantineSolutions 11),
-    (9801,1820) @=? (head $ diophantineSolutions 29)
+compressedSqrtExpansion :: Integer -> (Integer,[Integer])
+compressedSqrtExpansion = fsts . compress . sqrtFraction
+    where
+        fsts (x, xs) = (fst x, map fst xs)
+        fst (x, _, _) = x
+
+compress :: (Eq a) => [a] -> (a,[a])
+compress (x:x':xs) = (x, x' : takeWhile (/=x') xs)
+
+compressTest = [
+    (1, [1]) @=? (compress $ repeat 1),
+    (2, [3, 2]) @=? (compress $ cycle [2, 3])
     ]
 
-nthRootOrNothing :: Integer -> Integer -> Maybe Integer
-nthRootOrNothing = error "Not implemented"
+sqrtFraction :: Integer -> [(Integer, Integer, Integer)]
+sqrtFraction sq = iterate worker (0, 0, 1)
+    where
+        worker (l, n, d) = (l', n', d')
+            where
+                l' = findLargest (\x -> (d*x-n)^2 < sq) [0..]
+                n' = d * l' - n
+                d' = div (sq - n' * n') d
 
-nthRootOrNothingTest = [
-    Nothing @=? nthRootOrNothing 2 3,
-    Just 3 @=? nthRootOrNothing 3 27,
-    Just 4 @=? nthRootOrNothing 2 16
+sqrtFractionTest = [
+    (0,0,1) @=? (head $ sqrtFraction 2),
+    (1,1,1) @=? ((sqrtFraction 2) !! 1),
+    (2,1,1) @=? ((sqrtFraction 2) !! 2),
+    (2,1,1) @=? ((sqrtFraction 2) !! 3)
     ]
-    
+
+findLargest :: (a -> Bool) -> [a] -> a
+findLargest test (x:x':xs) = if test x' 
+    then findLargest test (x':xs)
+    else x
+
+findLargestTest = [
+    8 @=? findLargest (\x -> (1*x-4)^2<23) [0..],
+    1 @=? findLargest (\x -> (7*x-3)^2<23) [0..],
+    1 @=? findLargest (\x -> (7*x-4)^2<23) [0..]
+    ]
+
 unitTests = map TestCase $
-    nthRootOrNothingTest ++
-    diophantineSolutionsTest
-
+    findLargestTest ++
+    compressTest ++
+    sqrtFractionTest
+    
 data Arg = Euler | UnitTest |
     AdHoc {start :: Integer, stop :: Integer} 
     deriving (Show, Data, Typeable)
