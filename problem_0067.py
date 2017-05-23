5881a321c17e90da9c8b0b5cd496ae17206c883c
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-05-22 22:54:12 -0700

    Problem 67 updated to python3

--- problem_0067.py
+++ problem_0067.py
@@ -1,14 +1,34 @@
-#!/usr/bin/python
-"""http://projecteuler.net/problem=67"""
+#!/usr/bin/env python3
+"""
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
-from time import time
-from problem18 import add_up
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
+
+NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
+"""
+
+import logging
+import sys
+import time
+from problem_0018 import add_up
+
+logging.basicConfig(
+    stream=sys.stderr,
+    level=logging.INFO,
+    format='%(levelname)s: %(message)s')
 
 def main():
-	n_max = add_up('problem67.txt')
-	print "%d is the max path sum" % n_max
+    n_max = add_up('problem_0067.txt')
+    print("{} is the max path sum".format(n_max))
 
 if __name__ == "__main__":
-	start = time()
-	main()
-	print "That took %f seconds" %( time() - start )
+    start = time.time()
+    main()
+    logging.info("That took {:4.2f} seconds".format(time.time() - start))
