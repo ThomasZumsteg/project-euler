767d3ea16b0201d9525c432edf74156e09211abe
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-05-26 22:03:31 -0700

    Show and arms working

--- problem_0068.hs
+++ problem_0068.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
 
 import Test.HUnit ((@=?), runTestTT, Test(..))
-import Text.Printf (printf, PrintfArg)
+import Text.Printf (printf, PrintfArg, formatArg, formatString)
 import System.Console.CmdArgs
 
 import Common (exec, EulerArg, euler_main)
+
+import Data.List (intercalate)
 
 data Ngon = Ngon { items :: [Integer]} deriving (Show, Eq)
 
@@ -15,9 +17,21 @@ data Ngon = Ngon { items :: [Integer]} deriving (Show, Eq)
 -- 4,2,3; 5,3,1; 6,1,2
 
 arms :: Ngon -> [[Integer]]
-arms (Ngon{ items=items }) = [[i,i+1,half+i] | i <- [0..half]]
+arms (Ngon{ items=items }) = [[
+        items !! (half+i),
+        items !! (if i == (half-1) then 0 else (i+1)),
+        items !! (if i == 0 then (half-1) else i-1)] | 
+    i <- [0..(half-1)]]
     where
-        half = div (toInteger $ length items) 2
+        half = div (length items) 2
+
+testArms = [
+        [[4,2,3],[5,3,1],[6,1,2]] @=? arms (Ngon { items = [1,2,3,4,5,6] })
+    ]
+
+instance PrintfArg Ngon where
+    formatArg = formatString . 
+        intercalate "; " . map (intercalate "," . map show) . arms
 
 problem0068 :: Integer -> Ngon
 problem0068 = error "Not Implemented"
@@ -26,7 +40,7 @@ make_ngons :: Integer -> [Ngon]
 make_ngons = error "Not Implemeneted"
 
 unitTests = map TestCase $
-    []
+    testArms
     
 data Arg = Euler | UnitTest |
     AdHoc { ngon :: Integer } 
