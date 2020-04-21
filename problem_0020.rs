cfc9bc9600974fd57e019561a91988aaac186406
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2020-04-20 21:57:36 -0700

    Solution to problem 20 in rust

---
+++ problem_0020.rs
@@ -0,0 +1,24 @@
+#[macro_use]
+extern crate clap;
+extern crate chrono;
+
+use common::set_log_level;
+use log::info;
+use num::BigUint;
+
+fn main() {
+    let args = clap_app!(app =>
+        (about: "Solve Project Euler Problem 19, https://projecteuler.net/problem=19")
+        (@arg verbose: -v +multiple "Increase log level")
+        (@arg limit: "Power") 
+    ).get_matches();
+    set_log_level(&args);
+
+    let limit = args.value_of("limit").map(|n| n.parse::<usize>().unwrap()).unwrap_or(100);
+
+    let result = (1..limit).fold(BigUint::from(1usize), |acc, n| acc * BigUint::from(n));
+    info!("Result: {}", result);
+
+    let total: u32 = result.to_string().chars().map(|n| n.to_digit(10).unwrap()).sum();
+    println!("{}", total);
+}
