d2786af3f6107b05e162404832d8d8ac502d2d44
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2020-06-08 22:14:08 -0700

    More work on problem 60

--- problem_0060.rs
+++ problem_0060.rs
@@ -1,12 +1,13 @@
 #[macro_use]
 extern crate clap;
+extern crate maplit;
 
 use common::set_log_level;
 use common::primes::Primes;
 use log::{debug, info};
 use std::collections::{HashSet, HashMap};
 
-fn has_set_of(size: usize, root: usize, map: HashMap<usize, HashSet<usize>>) -> bool {
+fn find_valid_set(candidates: HashSet<usize>, map: &HashMap<usize, HashSet<usize>>, size: usize) -> Option<HashSet<usize>> {
     unimplemented!()
 }
 
@@ -23,21 +24,43 @@ fn main() {
 
     let mut primes = Primes::new();
     let mut concat_primes: HashMap<usize, HashSet<usize>> = HashMap::new();
-    for p in 1.. {
+    let mut result = None;
+    let mut p = 0;
+    while result.is_none() {
+        p += 1;
         let prime = primes.nth_prime(p);
         for q in 0..p {
             let qrime = primes.nth_prime(q);
             let crime = (prime.to_string() + &qrime.to_string()).parse::<usize>().unwrap();
             let drime = (qrime.to_string() + &prime.to_string()).parse::<usize>().unwrap();
             if primes.is_prime(crime) && primes.is_prime(drime) {
-                info!("{} + {} = {}, {} + {} = {}", qrime, prime, drime, prime, qrime, crime);
+                debug!("{} + {} = {}, {} + {} = {}", qrime, prime, drime, prime, qrime, crime);
                 concat_primes.entry(qrime).or_insert(HashSet::new()).insert(prime);
                 concat_primes.entry(prime).or_insert(HashSet::new()).insert(qrime);
-                if has_set_of(set_size, prime, concat_primes) {
-                    break;
-                }
+                let candidates = vec![prime, qrime].iter().map(|n| n.clone()).collect::<HashSet<usize>>();
+                result = find_valid_set(candidates, &concat_primes, set_size);
             }
         }
     }
-    unimplemented!();
+    let result = result.unwrap();
+    info!("{:?}", result);
+    println!("{}", result.iter().sum::<usize>());
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_has_set() {
+        assert_eq!(
+            hashmap!{
+                3 => hashset!{7, 109, 673},
+                7 => hashset!{3, 109, 673},
+                109 => hashset!{3, 7, 673},
+                673 => hashset!{3, 7, 109}
+            },
+            Some(hashset!{3, 7, 109, 673}),
+        )
+    }
 }
